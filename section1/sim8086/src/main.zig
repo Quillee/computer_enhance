8eaebccd35465bb1b21f115303cee3b88f5ed113
quillee <peraltamdb@gmail.com>
2023-04-25 05:53:45 -0400

    @dirty | still working on section 1

--- section1/sim8086/src/main.zig
+++ section1/sim8086/src/main.zig
@@ -12,6 +12,34 @@ const LogType = enum {
     ERROR,
 };
 
+fn get_register(width: u8, register:  usize) @TypeOf("AX") {
+    if (width == 1) {
+        return switch(register) {
+            1 => "AX",
+            2 => "CX",
+            3 => "DX",
+            4 => "BX",
+            5 => "SP",
+            6 => "BP",
+            7 => "SI",
+            8 => "DI",
+            else => unreachable
+        };
+    } else {
+        return switch(register) {
+            1 => "AL",
+            2 => "CL",
+            3 => "DL",
+            4 => "BL",
+            5 => "SH",
+            6 => "BH",
+            7 => "SH",
+            8 => "DH",
+            else => unreachable
+        };
+    }
+}
+
 fn log(log_type: LogType, message: []u8) void {
     const now = std.time.Timer.now();
     switch(log_type) {
@@ -24,56 +52,45 @@ fn log(log_type: LogType, message: []u8) void {
 }
 
 pub fn main() anyerror!void {
-    const file = try std.fs.openFileAbsolute("X:\\home\\courses\\computer_enhance\\section1\\sim8086\\instructions\\input", .{ .mode = std.fs.File.OpenMode.read_write });
+    // @rewrite: should be relative
+    const file = try std.fs.cwd().openFile("instructions/input_many", .{});
     defer file.close();
 
     var buffer: [100]u8 = undefined;
     var i: usize = 0;
     const bytes_read = try file.readAll(&buffer);
     var instruction_arr = buffer[0..bytes_read];
-    if (@rem(instruction_arr.len, @intCast(usize, 2)) == 0) {
+    if (@rem(instruction_arr.len, @intCast(usize, 2)) != 0) {
         std.debug.print("{d}", .{ @rem(instruction_arr.len, @intCast(usize, 2)) });
         return InputError.NonEvenInstructionSet;
     }
-
-    var decoded_str = "bits 16\n\n";
-    const word_registers = [_]u8{ "AX", "CX", "DX", "BX", "SP", "BP", "SI", "DI" };
-    const byte_registers = [_]u8{ "AL", "CL", "DL", "BL", "AH", "CH", "DH", "BH" };
 
     // @dumb: in reality instructions will decide the length of the input
     while (i < instruction_arr.len) : (i += 2) {
         const instruction = buffer[i] & 0xFC;
         switch (instruction) {
             136 => {
-                decoded_str += "mov ";
-                var registers: []u8 = undefined;
-                const d = buffer[i] & 0x02;
+                const d = buffer[i] & 0x02 > 0;
                 const w = buffer[i] & 0x01;
                 // 11000000
                 const mod = buffer[i + 1] & 0xC0;
                 // 00111000
-                const reg = buffer[i + 1] & 0x38;
+                const reg = (buffer[i + 1] & 0x38) >> 3;
                 // 00000111
                 const reg_mem = buffer[i + 1] & 0x7;
                 if (mod == 3) {
-                    log(LogType.ERROR, "mod provided doesn't represent register to register.\n");
+                    std.debug.print("[ERR] mod not working", .{});
                     break;
                 }
-                if (d == 1) {
-
-                }
-                if (w == 1) {
-                    registers = word_registers[0..];
-                } else {
-                    registers = byte_registers[0..];
-                }
-                const source = registers[reg];
-                const dest = registers[reg_mem];
-                std.debug.print("D bit: {d}, w bit: {d}, args: {d} ,source: {s}, dest: {s}", .{ d, w, instruction, source, dest });
+                
+                const source = get_register(w, if (d) reg else reg_mem);
+                const dest = get_register(w, if (d) reg_mem else reg);
+                // std.debug.print("D bit: {d}, w bit: {d}, args: {d} ,source: {s}, dest: {s}", .{ d, w, instruction, source, dest });
+                std.debug.print("mov {s}, {s}\n", .{ source, dest });
             },
-            else => std.debug.print("Unknown instruction", .{})
+            else => std.debug.print("Unknown instruction {d}\n", .{ instruction })
         }
     }
-    std.debug.print("File contents {s}\n", .{ instruction_arr });
+    // std.debug.print("File contents {s}\n", .{ instruction_arr });
 }
 
